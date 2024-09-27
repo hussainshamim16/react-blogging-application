6d3a0046d852c17785843c943c98220807b788d3
hussainshamim16 <hj422748@gmail.com>
2024-09-27 08:41:23 -0700

    start boolging

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,10 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { add,remove } from './config/redux/blogSlice'
-import Naver from './componants/Navbar'
-import Hero from './componants/Hero'
 
 const App = () => {
-  const count = useSelector((store)=> store.count.count)
-  const dispatch = useDispatch()
   return (
-  <div className="container-fluid">
-    <Naver/>
-    <Hero/>
-    <div className="third text-center mt-4">
-      <h3>Most Populer Blogs</h3>
-      
-    </div>
-    {/* <h1>count : {count}</h1>
-    <button onClick={()=> dispatch(add())}>add</button>
-    {count > 0 ? <button onClick={()=> dispatch(remove())}>remove</button> : null} */}
-  </div>
+    <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
   )
 }
 
